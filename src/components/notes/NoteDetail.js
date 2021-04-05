ef4e5b1c030f6da8826b10d7d184380a6da4fde8
dikshitakambri <dikshitakambri18@gmail.com>
2021-04-05 13:02:18 +0530

    refined the note detail page

--- src/components/notes/NoteDetail.js
+++ src/components/notes/NoteDetail.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import {useSelector} from 'react-dom';
+import {useFirestoreConnect, isLoaded, isEmpty} from 'react-redux-firebase';
+import moment from 'moment';
 
 const NoteDetail = (props) => {
 
-    const id = props.match.params.id
+    const id = props.match.params.id;
+    useFirestoreConnect([{collection:'notes',doc:id}]);
+    const note = useSelector(({firestore:{data}}) => data.notes && data.notes[id]);
     return(
-        <div>{id}</div>
+        <div className="container section">
+            <div className="card z-depth-0">
+                <div className="card-content">
+                    <span className="card-title">{note?.title}</span>
+                    <p>{note?.content}</p>
+                </div>
+                <div className="card-action grey lighten-4 grey-text">
+                    <div>{moment(note?.createdAt.toDate()).calendar()}</div>
+                </div>
+            </div>
+        </div>
     );
 }
 
