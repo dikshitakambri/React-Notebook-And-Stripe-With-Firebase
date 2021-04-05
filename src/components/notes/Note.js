90f1b95e968d6bd35059d6e1b8726445a0cd6066
dikshitakambri <dikshitakambri18@gmail.com>
2021-04-05 10:08:50 +0530

    delete note action added

--- src/components/notes/Note.js
+++ src/components/notes/Note.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import {deleteNote} from '../../store/Reducers/noteAction';
+import {useDispatch} from 'react-redux';
 
 const Note = ({note}) => {
+    const dispatch = useDispatch();
+    const deleteNoteHandler = () => {
+        dispatch(deleteNote(note))
+    }
+
     return(
         <div className="note white">
             <div className="right-align">
                 <i className="material-icons red-text" style={{cursor: 'pointer'}}>favorite</i>
-                <i className="material-icons" style={{cursor: 'pointer'}}>delete</i>
+                <i className="material-icons" style={{cursor: 'pointer'}} onClick={deleteNoteHandler} >delete</i>
             </div>
             <h5 className="black-text">{note.title}</h5>
             <p className="trucate">{note.content}</p>
--- src/store/Reducers/noteAction.js
+++ src/store/Reducers/noteAction.js
@@ -1,12 +1,25 @@
 export const addNote = (note) => {
     return(dispatch,getState, {getFirestore}) => {
         const firestore = getFirestore()
-        firestore.collection('notes')
+        firestore.collection('notes').doc(note.id).delete()
+
         .add({
             ...note,
             favourite: false,
             createdAt: new Date()
         })
+        .then(() => {
+            console.log('note aaded successfully');
+        })
+        .catch(err => {
+            console.log(err);
+        })
+    }
+}
+export const deleteNote = (note) => {
+    return(dispatch,getState, {getFirestore}) => {
+        const firestore = getFirestore()
+        firestore.collection('notes').doc(note.id).delete()
         .then(() => {
             console.log('note aaded successfully');
         })
